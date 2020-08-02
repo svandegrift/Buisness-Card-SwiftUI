4f4a0bab77aa9c0966840341dad98b97643ce388
Steven Vandegrift <svandegrift@icloud.com>
2020-08-02 12:11:50 -0500

    Made a Buisness Card
    Made with SwiftUI

--- StevenCard/ContentView.swift
+++ StevenCard/ContentView.swift
@@ -10,7 +10,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Color(red: 0.20, green: 0.60, blue: 0.86)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                
+                    Image("steven")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 200, height: 200, alignment: .center)
+                        .clipShape(Circle())
+                        .overlay(Circle().stroke(Color.white, lineWidth: 5))
+                
+                Text("Steven Vandegrift")
+                    .foregroundColor(.white)
+                    .font(Font.custom("FredokaOne-Regular", size: 30))
+                    .bold()
+                Text("iOS Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 20))
+                Divider()
+                InfoView(text: "svandegrift@icloud.com", imageName: "envelope.fill")
+                InfoView(text: "sjvandy.com", imageName: "desktopcomputer")
+            }
+        }
     }
 }
 
@@ -19,3 +42,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
---
+++ StevenCard/InfoView.swift
@@ -0,0 +1,34 @@
+//
+//  InfoView.swift
+//  StevenCard
+//
+//  Created by Steven Vandegrift on 8/2/20.
+//  Copyright © 2020 Steven Vandegrift. All rights reserved.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    
+    let text: String
+    let imageName: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25)
+            .fill(Color.white)
+            .frame(height: 50)
+            .overlay(HStack {
+                Image(systemName: imageName)
+                    .foregroundColor(.blue)
+                Text(text)
+            })
+            .padding(.all)
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView(text: "Hello", imageName: "phone.fill")
+            .previewLayout(.sizeThatFits)
+    }
+}
